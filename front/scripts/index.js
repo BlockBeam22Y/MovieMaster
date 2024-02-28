717d3a36819984944b7633b579bdd14133deefae
BlockBeam22Y <rodrigofc1910@gmail.com>
2024-02-28 03:12:16 -0500

    HW1 First layout

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,59 @@
-console.log(tempData);
+function createMovieCard(movie) {
+    const { title, year, director, duration, genre, rate, poster } = movie;
+
+    const cardLink = document.createElement('a');
+    cardLink.href = '';
+    cardLink.classList.add('movie-link');
+
+    const card = document.createElement('div');
+    card.classList.add('movie-card');
+
+    const cardTitle = document.createElement('h3');
+    cardTitle.innerHTML = title;
+    cardTitle.classList.add('movie-title');
+    card.appendChild(cardTitle);
+    
+    const cardImage = document.createElement('img');
+    cardImage.src = poster;
+    cardImage.alt = title;
+    cardImage.classList.add('movie-image');
+    card.appendChild(cardImage);
+
+    const cardYear = document.createElement('p');
+    cardYear.innerHTML = `Año: ${year}`;
+    cardYear.classList.add('movie-year');
+    card.appendChild(cardYear);
+
+    const cardDirector = document.createElement('p');
+    cardDirector.innerHTML = `Director: ${director}`;
+    cardDirector.classList.add('movie-director');
+    card.appendChild(cardDirector);
+
+    const cardDuration = document.createElement('p');
+    cardDuration.innerHTML = `Duración: ${duration}`;
+    cardDuration.classList.add('movie-duration');
+    card.appendChild(cardDuration);
+
+    const cardGenre = document.createElement('p');
+    cardGenre.innerHTML = `Géneros: ${genre.join(', ')}`;
+    cardGenre.classList.add('movie-genre');
+    card.appendChild(cardGenre);
+
+    const cardRate = document.createElement('p');
+    cardRate.innerHTML = `Calificación: ${rate}/10`;
+    cardRate.classList.add('movie-rate');
+    card.appendChild(cardRate);
+
+    cardLink.appendChild(card);
+
+    return cardLink;
+}
+
+const cardContainer = document.querySelector('.movie-container');
+cardContainer.innerHTML = '';
+
+const cards = tempData.map(createMovieCard);
+
+cards.forEach(card => {
+    cardContainer.appendChild(card);
+})
